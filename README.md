b559fb3f070f9d166c201c58bf7ac7b342a9f650
Adam Cmiel <acmiel1@gmail.com>
2023-09-02 21:04:27 +0200

    Initial commit
    Signed-off-by: Adam Cmiel <acmiel1@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arkruiter
+
+Arknights recruitment helper CLI.
---
+++ src/arkruiter/main.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main() -> None:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("tag", nargs="+", help="select a recruitment tag")
+    args = ap.parse_args()
+
+    tags: list[str] = args.tag
+    print("\n".join(tags))

836156d4848bdd50d2da47a7d2c36b5079fc7d85
Adam Cmiel <acmiel1@gmail.com>
2023-09-03 00:16:40 +0200

    Implement gacha table downloading
    Signed-off-by: Adam Cmiel <acmiel1@gmail.com>

---
+++ src/arkruiter/model/gacha_table.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import re
+from collections.abc import Sequence  # noqa: TCH003
+from typing import Self
+
+import pydantic
+
+from arkruiter.requests import download_json
+
+DEFAULT_URL = "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/en_US/gamedata/excel/gacha_table.json"
+
+
+class Tag(pydantic.BaseModel):
+    tag_name: str = pydantic.Field(alias="tagName")
+
+
+class GachaTable(pydantic.BaseModel):
+    gacha_tags: Sequence[Tag] = pydantic.Field(alias="gachaTags")
+    recruit_detail: str = pydantic.Field(alias="recruitDetail")
+
+    def is_recruitable(self, character_name: str) -> bool:
+        # Matches:
+        # <@rc.eml>'Justice Knight'</> <- 'Justice Knight'
+        # <@rc.eml>Yato</>             <- Yato
+        # / Fang /                     <- Fang
+        # / Spot$                      <- Spot
+        # ^Exusiai /                   <- Exusiai
+        pattern = rf"(^|/ |>){re.escape(character_name)}(<| /|$)"
+        return re.search(pattern, self.recruit_detail, re.MULTILINE) is not None
+
+    @classmethod
+    def from_url(cls, url: str = DEFAULT_URL) -> Self:
+        return cls.model_validate(download_json(url))
---
+++ tests/model/test_gacha_table.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import textwrap
+
+import pytest
+from arkruiter.model.gacha_table import GachaTable
+
+
+@pytest.mark.network
+def test_gacha_table_from_url() -> None:
+    table = GachaTable.from_url()
+    tag_names = [tag.tag_name for tag in table.gacha_tags]
+    assert tag_names == [
+        "Guard",
+        "Sniper",
+        "Defender",
+        "Medic",
+        "Supporter",
+        "Caster",
+        "Specialist",
+        "Vanguard",
+        "Melee",
+        "Ranged",
+        "Top Operator",
+        "Crowd-Control",
+        "Nuker",
+        "Senior Operator",
+        "Healing",
+        "Support",
+        "Starter",
+        "DP-Recovery",
+        "DPS",
+        "Survival",
+        "AoE",
+        "Defense",
+        "Slow",
+        "Debuff",
+        "Fast-Redeploy",
+        "Shift",
+        "Summon",
+        "Robot",
+        "Male",
+        "Female",
+    ]
+
+
+def test_is_recruitable() -> None:
+    # <@rc.eml>'Justice Knight'</> <- 'Justice Knight'
+    # <@rc.eml>Yato</>             <- Yato
+    # / Fang /                     <- Fang
+    # / Spot$                      <- Spot
+    # ^Exusiai /                   <- Exusiai
+    table = GachaTable(
+        gachaTags=[],
+        recruitDetail=textwrap.dedent(
+            """
+            <@rc.eml>'Justice Knight'</>
+            <@rc.eml>Yato</>
+            / Fang /
+            / Spot
+            Exusiai / Silver Ash
+            """
+        ),
+    )
+    assert table.is_recruitable("'Justice Knight'")
+    assert table.is_recruitable("Yato")
+    assert table.is_recruitable("Fang")
+    assert table.is_recruitable("Spot")
+    assert table.is_recruitable("Exusiai")
+    assert table.is_recruitable("Silver Ash")
+    assert not table.is_recruitable("Ash")

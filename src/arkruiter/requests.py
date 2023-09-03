2f4bdeb1ec9e4deb2ca1dbe5cc28f5dbd4fd6f19
Adam Cmiel <acmiel1@gmail.com>
2023-09-03 09:38:53 +0200

    Add requests caching
    The gacha table and character table data shouldn't be fully
    re-downloaded unless the content changes on the server.
    
    Signed-off-by: Adam Cmiel <acmiel1@gmail.com>

--- src/arkruiter/requests.py
+++ src/arkruiter/requests.py
@@ -3,18 +3,24 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
-import requests
+import requests_cache
 
 if TYPE_CHECKING:
     from arkruiter.types import JsonData
 
 log = logging.getLogger(__name__)
 
-_session = requests.session()
+_session = requests_cache.CachedSession(
+    "arkruiter",
+    serializer="json",
+    backend="filesystem",
+    use_cache_dir=True,  # store in <platform-specific config dir>/arkruiter/
+    cache_control=True,  # respect the cache-control header
+    expire_after=0,  # if no header, expire immediately (still do conditional requests)
+)
 
 
 def download_json(url: str) -> JsonData:
-    log.info("Downloading data from %s", url)
-    resp = _session.get(url, timeout=10)
+    resp = _session.get(url, timeout=10)  # type: ignore reportUnknownMemberType
     resp.raise_for_status()
     return resp.json()
